71be9e91e5118d9712dbacd17ceb084219e15da7
FathurRahman0 <rahmanfthr165@gmail.com>
2025-06-10 14:59:21 +0700

    membuat unlimited fule dalam mode menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -9,8 +9,19 @@ int main() {
     // membuka file dalam mode menulis
     ofstream outfile;
        // menunjuk ke sebuah nama file
-    outfile.open("data.txt");
+    outfile.open("contohfile.txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-
+    
+    // unlimited fule dalam mode menulis
+    while (true){
+        cout << "- ";
+        // mendapatkan setiap karakter dalam sati baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukkan kerakter q
+        if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    
 }
