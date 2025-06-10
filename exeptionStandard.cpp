19d09003a1d4abc243e2dfcb13e9fea0e9f94366
FathurRahman0 <rahmanfthr165@gmail.com>
2025-06-10 15:17:11 +0700

    membuat int main awal program

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,3 +4,14 @@
 #include <array>
 //untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; // penanda 1:...
+    try{
+        array<int, 3> data = {10, 20, 25};
+        //pesan arrat integer 3 elemen
+        cout << data.at(5) << endl;
+        //memanggil array integer elemen 5
+    }
+}
